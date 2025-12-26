9c476239506979cdc6cd1e0c31717e2f11d13465
SorerBOT <alonfiller@gmail.com>
2025-12-26 18:24:17 +0200

    FEAT added function signatures

---
+++ binary_tree.h
@@ -0,0 +1,24 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include <stdbool.h>
+
+typdef struct TreeNode
+{
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} TreeNode;
+
+TreeNode* createNode(int data);
+TreeNode* insertNode(TreeNode* root, int data);
+TreeNode* deleteNode(TreeNode* root, int data);
+TreeNode* searachNode(TreeNode* root, int data);
+TreeNode* searachNode(TreeNode* root, int data);
+TreeNode* findMin(TreeNode* root);
+void inorderTraversal(TreeNode* root);
+void preorderTraversal(TreeNode* root);
+void postorderTraversal(TreeNode* root);
+void freeTree(TreeNode* root);
+
+#endif /* BINARY_TREE_H */
