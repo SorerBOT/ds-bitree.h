ce183248486342ceb8c0c929cd57e3b6234266d6
SorerBOT <alonfiller@gmail.com>
2025-12-27 12:55:44 +0200

    FEAT added findMin test

--- tests/should_create_tree.c
+++ tests/should_create_tree.c
@@ -52,14 +52,42 @@ CUNIT_TEST(create_tree)
 CUNIT_TEST(min_node)
 {
     TreeNode* tree = createNode(10);
-    insertNode(tree, 5);
-    insertNode(tree, 15);
-    insertNode(tree, 3);
-    insertNode(tree, 7);
-    insertNode(tree, 12);
-    insertNode(tree, 18);
-
     TreeNode* minNode = findMin(tree);
     CUNIT_ASSERT_PTR_NOT_NULL(minNode);
+    CUNIT_ASSERT_INT_EQ(minNode->data, 10);
+
+    insertNode(tree, 5);
+    minNode = findMin(tree);
+    CUNIT_ASSERT_PTR_NOT_NULL(minNode);
+    CUNIT_ASSERT_INT_EQ(minNode->data, 5);
+
+    insertNode(tree, 15);
+    minNode = findMin(tree);
+    CUNIT_ASSERT_PTR_NOT_NULL(minNode);
+    CUNIT_ASSERT_INT_EQ(minNode->data, 5);
+
+    insertNode(tree, 3);
+    minNode = findMin(tree);
+    CUNIT_ASSERT_PTR_NOT_NULL(minNode);
     CUNIT_ASSERT_INT_EQ(minNode->data, 3);
+
+    insertNode(tree, 7);
+    minNode = findMin(tree);
+    CUNIT_ASSERT_PTR_NOT_NULL(minNode);
+    CUNIT_ASSERT_INT_EQ(minNode->data, 3);
+
+    insertNode(tree, 12);
+    minNode = findMin(tree);
+    CUNIT_ASSERT_PTR_NOT_NULL(minNode);
+    CUNIT_ASSERT_INT_EQ(minNode->data, 3);
+
+    insertNode(tree, 18);
+    minNode = findMin(tree);
+    CUNIT_ASSERT_PTR_NOT_NULL(minNode);
+    CUNIT_ASSERT_INT_EQ(minNode->data, 3);
+}
+
+CUNIT_TEST(search_node)
+{
+
 }
