37aecfc1afb1faef6ab218be2279a6ba34d64780
SorerBOT <alonfiller@gmail.com>
2025-12-28 15:56:10 +0200

    FEAT added thread_safe insertion test

---
+++ tests/should_be_thread_safe.c
@@ -0,0 +1,42 @@
+#include <omp.h>
+#include <stdio.h>
+
+#include "../external/cunit.h"
+#include "../binary_tree.h"
+
+int is_valid_tree(TreeNode* root)
+{
+    int is_valid = true;
+    if (root == NULL)
+    {
+        return true;
+    }
+    if (root->left != NULL)
+    {
+        is_valid = is_valid && root->left->data <= root->data && is_valid_tree(root->left);
+    }
+    if (root->right != NULL)
+    {
+        is_valid = is_valid && root->right->data > root->data && is_valid_tree(root->right);
+    }
+
+    return is_valid;
+}
+
+CUNIT_TEST(thread_safe_insertion)
+{
+    TreeNode* tree = NULL;
+#pragma omp parallel for schedule(static, 6)
+    for (size_t i = 0; i < 100; ++i)
+    {
+        insertNode(tree, i);
+    }
+
+    CUNIT_ASSERT_TRUE(is_valid_tree(tree));
+    int did_insert_everything = 1;
+#pragma omp parallel for schedule(static, 6) reduction(&&:did_insert_everything)
+    for (size_t i = 0; i < 100; ++i)
+    {
+        did_insert_everything = did_insert_everything && searchNode(tree, i);
+    }
+}
