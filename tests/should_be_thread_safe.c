a08c70f9eafcb15738862d7be923d6b7461d09ea
SorerBOT <alonfiller@gmail.com>
2025-12-28 17:44:58 +0200

    FEAT added test for search

--- tests/should_be_thread_safe.c
+++ tests/should_be_thread_safe.c
@@ -48,8 +48,12 @@ CUNIT_TEST(thread_safe_deletion)
     {
         insertNode(tree, i);
     }
+    /*
+     * This does not test cases in which the root is deleted. This would be impossible to check, as we don't support that functionality
+     * To support this functionality, one would have to use a fake-parent for the root node, which can actually be locked and unlocked.
+     */
 #pragma omp parallel for schedule(static, 6)
-    for (size_t i = 0; i < 100; ++i)
+    for (size_t i = 1; i < 100; ++i)
     {
         if (i % 3 == 0)
         {
@@ -58,7 +62,7 @@ CUNIT_TEST(thread_safe_deletion)
     }
 
     CUNIT_ASSERT_TRUE(is_valid_tree(tree));
-    for (size_t i = 0; i < 100; ++i)
+    for (size_t i = 1; i < 100; ++i)
     {
         if (i % 3 == 0)
         {
@@ -73,5 +77,42 @@ CUNIT_TEST(thread_safe_deletion)
 
 CUNIT_TEST(thread_safe_search)
 {
+    TreeNode* tree = createNode(0);
+    for (size_t i = 1; i < 100; ++i)
+    {
+        if (i % 3 == 0)
+        {
+            insertNode(tree, i);
+        }
+    }
 
+    size_t N = 100;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            #pragma omp taskloop nogroup 
+            for (size_t i = 1; i < N; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    insertNode(tree, i);
+                }
+            }
+
+            // Loop 2
+            #pragma omp taskloop nogroup
+            for (size_t j = 1; j < N; j++)
+            {
+                deleteNode(tree, j);
+            }
+
+            // Loop 3
+            #pragma omp taskloop nogroup
+            for (size_t k = 1; k < N; k++)
+            {
+                searchNode(tree, k);
+            }
+        }
+    }
 }
